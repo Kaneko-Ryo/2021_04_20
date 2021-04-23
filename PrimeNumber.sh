4e71565f57595e3f25a359fa5282fd8fa8cd0194
Kaneko_Ryo <r.kaneko@amata.co.jp>
2021-04-23 16:24:24 +0900

    fix

--- PrimeNumber.sh
+++ PrimeNumber.sh
@@ -3,12 +3,12 @@
 set +x #コマンドの非表示
 value=$1
 errorMsg="This Value is Error" #エラーメッセージ
-expr $value + 1 > /dev/null 2>&1
-ret=$?
+expr $value + 1 > /dev/null 2>&1 #正しい値かどうか判定
+ret=$? #判定結果の取得
 
 if [ $((ret)) -lt $((2)) ]; then #正しい値
 #正しい値の時
-if [ $((value)) -gt $((1)) ]; then #2以上の時
+#if [ $((value)) -gt $((1)) ]; then #2以上の時
 #2からvalueまで繰り返し
     for ((i=2 ; i<=$((value)) ; i++))
     do
@@ -32,6 +32,6 @@ if [ $((value)) -gt $((1)) ]; then #2以上の時
     done
 else #間違い値の時
 echo $errorMsg #エラーメッセージ出力
-fi
+#fi
 fi
 set -x #コマンドの非表示解除
