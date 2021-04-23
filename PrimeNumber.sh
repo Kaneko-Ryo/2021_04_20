8b21ea6194693c684e42482956e3f5272d76ceba
Kaneko_Ryo <r.kaneko@amata.co.jp>
2021-04-23 13:56:26 +0900

    Add PrimeNumber.sh

---
+++ PrimeNumber.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+set +x #コマンドの非表示
+value=$1
+errorMsg="This Value is Error" #エラーメッセージ
+
+#正しい値の時
+if [ $((value)) -gt $((1)) ]; then #2以上の時
+#2からvalueまで繰り返し
+    for ((i=2 ; i<=$((value)) ; i++))
+    do
+            if [ $((i)) -gt $((2)) ]; then #iが2より大きいとき
+                #2からiまで繰り返し
+                for ((j=2 ; $((j))<$((i)) ; j++))
+                do
+                    n=$((i%j)) #割り切れたら素数じゃない
+                    if [ $((n)) -eq $((0)) ]; then
+                        break #ループ終了
+                    fi
+                    
+                    #最後まで割り切れなかったら素数
+                    if [ $((i-1)) -eq $((j)) ]; then
+                    echo $i #出力
+                    fi
+                done
+            elif [ $((i)) -eq $((2)) ]; then #iが2の時
+                echo $i #2を出力
+            fi
+    done
+else #間違い値の時
+echo $errorMsg #エラーメッセージ出力
+fi
+set -x #コマンドの非表示解除
